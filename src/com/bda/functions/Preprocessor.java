ac49362a46324adb15497aff3d38e9f32b67163d
Said <saidozcn@gmail.com>
2015-12-13 15:46:50 +0200

    Fixes a bug on replacing urls

--- src/com/bda/functions/Preprocessor.java
+++ src/com/bda/functions/Preprocessor.java
@@ -4,11 +4,10 @@ import java.util.regex.Matcher;
 
 public class Preprocessor {
 
-    private static final String urlToken = "%HTTPADDR%";
-    private static final String emailToken = "%EMAILADDR%";
-    private static final String numericToken = "%NUMERIC%";
+    private static final String urlToken = "TOKENIZEDHTTPADDR";
+    private static final String emailToken = "TOKENIZEDEMAILADDR";
 
-    private static final String urlRegexp = "^(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?$";
+    private static final String urlRegexp = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
     private static final String emailRegexp = "([^.@\\s]+)(\\.[^.@\\s]+)*@([^.@\\s]+\\.)+([^.@\\s]+)";
     private static final String nonAlphanumericRegexp = "[^a-zA-Z0-9\\s]";
     private static final String numericRegexp = "\\d{1,}";
@@ -28,7 +27,8 @@ public class Preprocessor {
 
         cleanedText = removeNonAlphanumericCharacters(cleanedText);
         cleanedText = removeSpecialCharacters(cleanedText);
-        return cleanedText;
+
+        return cleanedText.toLowerCase();
     }
 
     private static String removeHTML(String text){
@@ -44,7 +44,7 @@ public class Preprocessor {
     }
 
     private static String tokenizeNumericCharacters(String text){
-        return text.replaceAll(numericRegexp, numericToken);
+        return text.replaceAll(numericRegexp, " ");
     }
 
     private static String removeNonAlphanumericCharacters(String text){
